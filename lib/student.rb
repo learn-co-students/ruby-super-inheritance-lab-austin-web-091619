9f56dd559975ce5f533b6f77c857e9cfc2bcb9a1
swdecker <swdecker@gmail.com>
2019-09-23 15:37:52 -0500

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,10 @@
+class Student
+    def hello
+    # Writ #hello, that puts out the phrase:
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+    def raise_hand
+        puts "Pick me!"
+    end
+
+end
